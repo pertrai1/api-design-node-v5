1c8f41d6f9015bcfadf0d36c5addc8e897cef8ef
Rob Simpson <rsimpson2@me.com>
2025-08-14 15:23:38 -0400

    targetCount string to number

--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -18,7 +18,7 @@ const createHabitSchema = z.object({
   name: z.string(),
   description: z.string().optional(),
   frequency: z.string(),
-  targetCount: z.string(),
+  targetCount: z.number(),
   tagIds: z.array(z.string()).optional(),
 })
 
